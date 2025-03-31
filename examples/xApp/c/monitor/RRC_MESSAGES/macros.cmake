6ef964acd5d759696c885f99ef44882f831a6140
Teodora <teodora.vladic@openairinterface.org>
2025-04-01 00:32:13 +0200

    Delete irrelevant check_option() function from cmake
    Accidentally copied from OAI RAN repository.

--- examples/xApp/c/monitor/RRC_MESSAGES/macros.cmake
+++ examples/xApp/c/monitor/RRC_MESSAGES/macros.cmake
@@ -26,22 +26,3 @@ function(make_version VERSION_VALUE)
   endforeach()
   set(${VERSION_VALUE} "${RESULT}" PARENT_SCOPE)
 endfunction()
-
-function(check_option EXEC TEST_OPTION EXEC_HINT)
-  message(STATUS "Check if ${EXEC} supports ${TEST_OPTION}")
-  execute_process(COMMAND ${EXEC} ${TEST_OPTION}
-                  RESULT_VARIABLE CHECK_STATUS
-                  OUTPUT_VARIABLE CHECK_OUTPUT
-                  ERROR_VARIABLE CHECK_OUTPUT)
-  if(NOT ${CHECK_STATUS} EQUAL 1)
-    get_filename_component(EXEC_FILE ${EXEC} NAME)
-    message(FATAL_ERROR "Error message: ${CHECK_OUTPUT}\
-You might want to re-run ./build_oai -I
-Or provide a path to ${EXEC_FILE} using
-  ./build_oai ... --cmake-opt -D${EXEC_HINT}=/path/to/${EXEC_FILE}
-or directly with
-  cmake .. -D${EXEC_HINT}=/path/to/${EXEC_FILE}
-")
-  endif()
-endfunction()
-
